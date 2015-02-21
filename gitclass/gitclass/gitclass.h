33815c8c1cce2f70a3ecb9819e249057c515285c
Antoha <qwert@MacBook-Air-Antoha.local>
2015-02-21 16:02:32 +0300

    2

---
+++ gitclass/gitclass/gitclass.h
@@ -0,0 +1,7 @@
+class gitclass
+{protected:
+    int x=0;
+public:
+    virtual int show()const{return x;}
+};
+
---
+++ gitclass/gitclass/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gitclass.h"
+
+using namespace std;
+
+int main()
+{
+    gitclass a;
+    cout<<a.show()<<endl;
+    return 0;
+}
