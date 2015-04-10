e51d9d504e2336fb67c6ec566bd20e9474fbcdf1
Antoha <qwert@MacBook-Air-Antoha.local>
2015-04-10 22:00:57 +0300

    no message

--- 16 thread/main.cpp
+++ 16 thread/main.cpp
@@ -16,9 +16,9 @@ void PI(int i, int N)
 {
     for(int j=14*i/N; j<14*(i+1)/N; ++j)
     {
-        std::unique_lock<std::mutex> lock(mutex, std::defer_lock);
-        lock.lock();
-        pi = pi + (1.0/pow(16.0, j))*((double)8/(8*j+2)+(double)4/(8*j+3)+(double)4/(8*j+4)-(double)1/(8*j+7))/2;
+        std::unique_lock<std::mutex> lock(mutex);//, std::defer_lock);
+//        lock.lock();
+        pi = pi + (1.0/pow(16.0, j))*(8.0/(8*j+2)+4.0/(8*j+3)+4.0/(8*j+4)-1.0/(8*j+7))/2;
         lock.unlock();
     }
 
