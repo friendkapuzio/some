76c51b0bf154a9a7ab14fff2c8e50b854d9e6086
Antoha <qwert@MacBook-Air-Antoha.local>
2015-04-11 18:39:15 +0300

    balancing

--- 16 thread/main.cpp
+++ 16 thread/main.cpp
@@ -12,26 +12,23 @@ double pi(0);
 
 std::mutex mutex;
 
-std::pair<int,int> blocks[10];
+std::pair<int,int> blocks[1000];
 
 std::atomic<int> curBlock(0);
 
 void PI()
 {
-    int block=0;
-    if(curBlock!=0)
-    {
-        //std::unique_lock<std::mutex> lock(mutex);
-        block=--curBlock;
-        //lock.unlock();
-    }
     double sum=0;
-    for(long j=blocks[block].first; j<blocks[block].second; ++j)
+    int block;
+    while(curBlock!=0)
     {
-        sum+=(1.0/pow(16.0, j))*(8.0/(8*j+2)+4.0/(8*j+3)+4.0/(8*j+4)-1.0/(8*j+7))/2;
+        block=--curBlock;
+        for(long j=blocks[block].first; j<blocks[block].second; ++j)
+        {
+            sum+=(1.0/pow(16.0, j))*(8.0/(8*j+2)+4.0/(8*j+3)+4.0/(8*j+4)-1.0/(8*j+7))/2;
+        }
     }
-    std::unique_lock<std::mutex> lock(mutex);//, std::defer_lock);
-    //        lock.lock();
+    std::unique_lock<std::mutex> lock(mutex);
     pi = pi + sum;
     lock.unlock();
 }
@@ -44,12 +41,11 @@ int main()
     std::vector<std::thread> threads;
     int N;
     std::cin>>N;
-    for (int i=0; i<N; ++i)
+    for (int i=0; i<1000; ++i)
     {
-        blocks[i]=std::make_pair(1400000*i/N, 1400000*(i+1)/N);
+        blocks[i]=std::make_pair(140000*i, 140000*(i+1));
         ++curBlock;
     }
-    
     auto start=std::chrono::system_clock::now();
     for (int i=0; i<N; ++i)
         threads.push_back(std::thread(PI));
